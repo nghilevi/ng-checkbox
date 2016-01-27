90dbb63eb25f34ba98bb65abb5e46bccf5d85361
vinhnghi223 <vinhnghi223@gmail.com>
2016-01-27 05:42:16 +0200

    Added copy:build task

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,23 @@ gulp.task('concat',['clean:build'],function(){
 			.pipe(gulp.dest(build));
 });
 
+gulp.task('copy:build',function(){
+	var vendorCssStream = gulp.src([
+		'node_modules/foundation-sites/dist/foundation.css',
+		'node_modules/foundation-sites/dist/foundation-flex.css'
+	]);
+	vendorCssStream.pipe(gulp.dest('demo/css'));
+
+	var vendorJSStream = gulp.src([
+		'node_modules/jquery/dist/jquery.min.js',
+		'node_modules/foundation-sites/dist/foundation.min.js',
+		'node_modules/angular/angular.min.js',
+		'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+		'node_modules/lodash/index.js'
+	]);
+	return vendorJSStream.pipe(gulp.dest('demo/js'));
+});
+
 gulp.task('dist',['clean:dist'],function(){
 	return gulp.src('demo/build/ngCheckbox.js')
 		.pipe($.ngAnnotate())
@@ -40,8 +57,8 @@ gulp.task('watch',['concat'],function(){
 	gulp.watch(['src/**/*.js','src/**/*.html'],['concat']);
 });
 
-gulp.task('default',['watch'],function(){
-	gulp.src(['demo','node_modules'])
+gulp.task('default',['copy:build','watch'],function(){
+	gulp.src(['demo'])
 		.pipe($.webserver({
 			livereload:true,
 			port:8000,
